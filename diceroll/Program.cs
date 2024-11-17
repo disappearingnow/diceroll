29f9112135e6fd989a4632864161ef4960caeb44
disappearingnow <disappearing.now@gmail.com>
2024-11-17 16:46:06 -0500

    Test DiceRoller class

--- diceroll/Program.cs
+++ diceroll/Program.cs
@@ -1,4 +1,11 @@
 ﻿/*
  1. Game rolls dice behind the scenes
-2. 
+ 2. Console prompts user for 3 attempts
+ 3. If user guess the dice's value correctly, they win
+ 4. If user fails to guess correct dice value, they lose
+ 5. Game ends
  */
+using diceroll.DiceRoller;
+
+int roll = DiceRoller.RollDice();
+Console.WriteLine(roll);
