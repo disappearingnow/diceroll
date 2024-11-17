fbafa2ee05ad3ad98e8b41d692f41327ed045bf5
disappearingnow <disappearing.now@gmail.com>
2024-11-17 17:58:27 -0500

    Created MessagePrinter class

---
+++ diceroll/MessagePrinter/MessagePrinter.cs
@@ -0,0 +1,26 @@
+﻿namespace diceroll.MessagePrinter;
+
+static class MessagePrinter
+{
+    const string WelcomeMessage = "Welcome to the dice roll game.  A dice has just been rolled.  Guess that value by inputting a number between 1 and 6.  You have three attempts";
+    const string InputPrompt = "Enter a number between 1 and 6.";
+    const string WinMessage = "Correct!  You have won the game.";
+    const string LossMessage = "You lose.  Please try again!";
+
+    public static void PrintWelcome()
+    {
+        Console.WriteLine(WelcomeMessage);
+    }
+    public static void PrintInputPrompt()
+    {
+        Console.WriteLine(InputPrompt);
+    }
+    public static void PrintWinMessage()
+    {
+        Console.WriteLine(WinMessage);
+    }
+    public static void PrintLossMessage()
+    {
+        Console.WriteLine(LossMessage);
+    }
+}
