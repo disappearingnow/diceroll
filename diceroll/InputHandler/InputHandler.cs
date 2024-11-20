0c28e2009cd655aff488e27c3b1100797c6c9981
disappearingnow <disappearing.now@gmail.com>
2024-11-20 16:26:08 -0500

    Created InputHandler class

---
+++ diceroll/InputHandler/InputHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace diceroll.InputHandler;
+
+using diceroll.MessagePrinter;
+
+internal class InputHandler
+{
+    public static int TakeInput()
+    {
+        bool passes;
+        int num;
+        do
+        {
+            MessagePrinter.PrintInputPrompt();
+            passes = int.TryParse(Console.ReadLine(), out num);
+
+
+            if (passes)
+            {
+                if (num < 1 || num > 6)
+                {
+                    passes = false;
+                    continue;
+                }
+            }
+
+        } while (!passes);
+
+        return num;
+    }
+}
+
