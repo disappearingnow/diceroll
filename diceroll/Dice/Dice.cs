15b6cbb57f67b29b8379a7923e24d51575309f88
disappearingnow <disappearing.now@gmail.com>
2024-11-21 15:55:02 -0500

    Renamed and modified DiceRoller class

---
+++ diceroll/Dice/Dice.cs
@@ -0,0 +1,19 @@
+﻿namespace diceroll.Dice;
+
+public class Dice
+{
+    private readonly Random _random;
+    private readonly int _rollValue;
+    private const int _sides = 6;
+    public Dice()
+    {
+        _random = new Random();
+        _rollValue = _random.Next(1, _sides + 1);
+    }
+
+    public int RollValue
+    {
+        get => _rollValue;
+    }
+
+}
